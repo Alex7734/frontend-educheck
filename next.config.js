4159a2f98ff4606d88265f4eb263aff4498dac00
Alex <alexmihoc@yahoo.com>
2025-04-22 23:53:38 +0300

    chore: add config

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:13000/api/:path*' // Proxy to Backend Server (NestJS)
+        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:13000'}/api/:path*`
       }
     ];
   },
